2f016e6b43f6c970f1e96e166b6c54f50fe9d93a
AnhMH <anh.mh@moziasoft.com>
2020-01-30 16:46:16 +0700

    Add controller

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
 const functions = require('firebase-functions');
+const express = require('express');
+
+const app = express();
+app.get('/t', (req, res) => {
+	res.send(`${Date.now()}`);
+});
+
+app.get('/tc', (req, res) => {
+	res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+	res.send(`${Date.now()}`);
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.app = functions.https.onRequest(app);
