ca1ac3c9d22358f16634296dcba71386a1ec8421
AnhMH <anh.mh@moziasoft.com>
2020-01-30 17:07:30 +0700

    Add view engines

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,34 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
 const express = require('express');
+const engines = require('consolidate');
+
+const firebaseApp = firebase.initializeApp(
+	functions.config().firebase
+);
+
+// function getFacts() {
+// 	const ref = firebaseApp.database().ref('facts');
+// 	return ref.once('value').then(snap => snap.val());
+// }
 
 const app = express();
+app.engine('hbs', engines.handlebars);
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+	res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+	// getFacts.then(facts => {
+		
+	// });
+	let facts = [
+		{'text' : 'aaa'},
+		{'text' : 'bbb'}
+	];
+	res.render('index', { facts });
+});
+
 app.get('/t', (req, res) => {
 	res.send(`${Date.now()}`);
 });
